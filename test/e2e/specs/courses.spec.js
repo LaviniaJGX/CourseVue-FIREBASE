758730e98d42ca254e112933801ec0ee38ce981e
LaviniaJGX <840762603@qq.com>
2018-12-10 01:37:02 +0800

    update: courses.spec.js

--- test/e2e/specs/courses.spec.js
+++ test/e2e/specs/courses.spec.js
@@ -1,39 +1,4 @@
 const baseURL = 'http://endend.herokuapp.com'
-describe('test the edit', () => {  
-  beforeEach(() => {
-    // Delete all courses in the API's datastore
-    cy.request(`${baseURL}/courses/`)
-      .its('body')
-      .then( (courses) => {
-         courses.forEach( (element) => {
-            cy.request('DELETE',
-            `${baseURL}/courses/${element._id}`)
-         });
-      })
-      // Populate API's datastore
-      cy.fixture('courses')
-        .then((courses) => {
-            courses.forEach((course) => {
-              cy.request('POST',
-              `${baseURL}/courses/`, course )
-            })
-      })
-      cy.visit("/");
-      // Click Manage courses navbar link
-      cy.get('.navbar-nav:nth-child(1)').
-          find('.nav-item:nth-child(2)').click()
-
- });
-  it("allows a course to be deleted", () => {
-    cy.get('tbody').find('tr').should('have.length', 4)
-    // Click trash/delete link of 3rd course in list
-    cy.get('tbody').find('tr:nth-child(3)').find('td:nth-child(9)').click()
-    // Click confirmation button
-    cy.get('button').contains('Delete').click()
-    cy.get('tbody').find('tr').should('have.length', 3)
-    cy.get('.swal2-confirm').click()
-  })
-})
 describe('test edit', () => {
   beforeEach(() => {
     cy.request(`${baseURL}/courses`)
